2ca9aeeb8eae22f2f7ba6ac26a2aa5c50ca73552
ushahid1 <ushahid@live.com>
2023-09-14 11:01:46 +0500

    server client files

---
+++ clientWord2vec.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class clientWord2vec
+{
+ 
+    private static Socket socket;
+ 
+    public static void main(String args[])
+    {
+        try
+        {
+            String host = "localhost";
+            int port = 10004;
+            InetAddress address = InetAddress.getByName(host);
+            socket = new Socket(address, port);
+ 
+            //Send the message to the server
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+ 
+            // number = "2";
+ 
+            String sendMessage = args[0] + "," + args[1];
+            bw.write(sendMessage);
+            bw.flush();
+            System.out.println("Message sent to the server : "+sendMessage);
+ 
+            //Get the return message from the server
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+			
+			
+	         
+
+	        //System.out.println("All the elements in the buffer:");
+
+	         
+
+	        
+			BufferedReader br = new BufferedReader(isr);
+            String message = br.readLine();
+            System.out.println("Message received from the server : " +message);
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+        finally
+        {
+            //Closing the socket
+            try
+            {
+                socket.close();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
